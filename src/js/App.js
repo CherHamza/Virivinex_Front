87c4ecd317322ac5af9b74f38e8f3e22802dc9fe
CherHamza <hamzacheracher1@hotmail.fr>
2023-12-18 10:05:33 +0100

    working on create logic

--- src/js/App.js
+++ src/js/App.js
@@ -11,7 +11,7 @@ function App() {
             <Link to="/app/index.html">Accueil</Link>
           </li>
           <li>
-            <Link to="/home">Page d'accueil</Link>
+            <Link to="/Home">Page d'accueil</Link>
           </li>
         </ul>
       </nav>
