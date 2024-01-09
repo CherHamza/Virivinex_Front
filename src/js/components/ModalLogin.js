7982681cd148e50b328c2c29e7caad903ff011c7
CherHamza <hamzacheracher1@hotmail.fr>
2024-01-09 15:08:20 +0100

    test to insert secondary entry point

--- src/js/components/ModalLogin.js
+++ src/js/components/ModalLogin.js
@@ -61,8 +61,8 @@ const ModalLogin = ({ handleShowLoginModal, handleCloseLoginModal, userType }) =
             <button type="button" className="btn btn-secondary" onClick={handleCloseLoginModal}>
               Cancel
             </button>
-      </div>
-         
+        
+        </div> 
       </div>
     </div>
   </div>
