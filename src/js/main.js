f7b997e38ecaa4cac27829b38822ff75f788c76d
CherHamza <hamzacheracher1@hotmail.fr>
2024-01-11 15:44:59 +0100

    adding footer and fix bugs

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,7 @@ import IndiviudalProfil from "./components/Individual/IndividualProfil.js";
 import ExpertProfil from "./components/Expert/ExpertProfil.js";
 import ProducerProfil from "./components/Producer/ProducerProfil.js";
 import { Header } from './components/Header.js';
+import { Footer } from './components/footer.js';
 
 
 
@@ -28,6 +29,7 @@ root.render(
         <Route path="/app/account/expert.html" element={<ExpertProfil/>}/>
         <Route path="/app/account/producer.html" element={<ProducerProfil />}/>
       </Routes>
+      <Footer />
     </Router>
     <ToastContainer />
   </React.StrictMode>
