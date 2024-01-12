f84789e87e79d0eefeacd4e8b91986cb368082b5
lana-12 <lana.12@wanadoo.fr>
2024-01-12 16:27:12 +0100

    Creation of each user type ok

--- src/js/components/Individual/IndividualModalCreate.js
+++ src/js/components/Individual/IndividualModalCreate.js
@@ -1,106 +1,123 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import ReactModal from "react-modal";
 import { toast } from 'react-toastify';
 import { dataService } from "../../services/dataService";
 
-
-
 const IndividualModalCreate = ({ handleShowModal, handleCloseModal, userType }) => {
-    const [email, setEmail] = useState("");
-    const [pseudo, setPseudo] = useState("");
-    const [password, setPassword] = useState("");
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [phone, setPhone] = useState("");
-    const [houseNumber, setHouseNumber] = useState("");
-    const [city, setCity] = useState("");
-    const [street, setStreet] = useState("");
-    const [postalCode, setPostalCode] = useState("");
-    const [countryCode, setCountryCode] = useState("");
-    const [countryName, setCountryName] = useState("");
-    const [salutation, setSalutation] = useState("");
+    const [formData, setFormData] = useState({
+        email: "",
+        pseudo: "",
+        password: "",
+        firstName: "",
+        lastName: "",
+        phone: "",
+        houseNumber: "",
+        city: "",
+        street: "",
+        postalCode: "",
+        countryCode: "",
+        countryName: "",
+        salutation: ""
+    });
 
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            [name]: value,
+        }));
+    };
 
     const handleCreateUser = () => {
-        if (!firstName.trim()) {
-            toast.error("Firstname required.");
+        // Validation des champs
+        let errorMessage = "";
+
+        switch (true) {
+            case !formData.firstName.trim():
+                errorMessage = "FirstName is required.";
+                break;
+            case !formData.lastName.trim():
+                errorMessage = "LastName is required.";
+                break;
+            case !formData.email.trim():
+                errorMessage = "Email is required.";
+                break;
+            case !formData.pseudo.trim():
+                errorMessage = "Pseudo is required.";
+                break;
+            case !formData.password || formData.password.length < 8:
+                errorMessage = "The password must contain at least 8 characters.";
+                break;
+            default:
+                break;
+        }
+
+        if (errorMessage) {
+            toast.error(errorMessage);
             return;
         }
-        if (!lastName.trim()) {
-            toast.error("Lastname required.");
-            return;
-        }
-        if (!email.trim()) {
-            toast.error("Email required.");
-            return;
-        }
-        if (!pseudo.trim()) {
-            toast.error("Pseudo required.");
-            return;
-        }
-        if (!password || password.length < 8) {
-            toast.error("The password must contain at least 8 characters.");
-            return;
-        }
-        
-       
+
         const newUser = {
             address: {
-                houseNumber,
-                city,
-                street,
-                postalCode,
+                houseNumber: formData.houseNumber,
+                city: formData.city,
+                street: formData.street,
+                postalCode: formData.postalCode,
                 country: {
-                    code: countryCode,
-                    name: countryName,
-                    language: "" 
+                    code: formData.countryCode,
+                    name: formData.countryName,
+                    language: ""
                 }
             },
-            companyName: "",
+            companyName: formData.firstName + " " + formData.lastName,
+            
             profiles: [
                 {
-                    firstName: firstName,
-                    lastName: lastName,
-                    phone: phone,
-                    mobilePhone: "",
-                    emailAddress: email,
+                    firstName: formData.firstName,
+                    lastName: formData.lastName,
+                    phone: formData.phone,
+                    mobilePhone: formData.phone,
+                    emailAddress: formData.email,
                     emailNotifications: false,
-                    salutation: salutation,
+                    salutation: formData.salutation,
                     user: {
-                        username: email,
-                        password: password
+                        username: formData.email,
+                        password: formData.password
                     },
                     metaInfo: {
-                        "pseudo": pseudo,
+                        "pseudo": formData.pseudo,
                         "userType": "Individual"
                     }
                 }
             ],
         };
-        dataService.createUser(newUser).then(res => {
-            console.log(res);
-            toast.success("Utilisateur créé avec succès !");
-            setFirstName("");
-            setLastName("");
-            setEmail("");
-            setPseudo("");
-            setPassword("");
-            setPhone("");
-            setHouseNumber("");
-            setCity("");
-            setStreet("");
-            setPostalCode("");
-            setCountryCode("");
-            setCountryName("");
-            setSalutation("");
-            handleCloseModal();
-        })
-        .catch(err => {
-            console.error(err);
-            toast.error("Erreur lors de la création de l'utilisateur.");
-        });
-    }
-      
+        console.log("newUser to be sent:", newUser);
+        dataService.createUser(newUser)
+            .then(res => {
+                console.log(res);
+                toast.success("User successfully created !");
+                setFormData({
+                    email: "",
+                    pseudo: "",
+                    password: "",
+                    firstName: "",
+                    lastName: "",
+                    phone: "",
+                    houseNumber: "",
+                    city: "",
+                    street: "",
+                    postalCode: "",
+                    countryCode: "",
+                    countryName: "",
+                    salutation: ""
+                });
+                handleCloseModal();
+            })
+            .catch(err => {
+                console.error(err);
+                toast.error("Error during user creation.");
+            });
+    };
 
     return (
         <div className={`modal fade ${handleShowModal ? "show" : ""}`} tabIndex="-1" role="dialog" style={{ display: handleShowModal ? "block" : "none", backgroundColor: "rgba(0,0,0,0.5)" }}>
@@ -115,64 +132,62 @@ const IndividualModalCreate = ({ handleShowModal, handleCloseModal, userType }) 
                     <div className="modal-body" style={{ backgroundColor: "#F2F2F2" }}>
                         <div className="form-group">
                             <label htmlFor="firstName">Firstname</label>
-                            <input type="text" className="form-control" id="firstName" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+                            <input type="text" className="form-control" id="firstName" name="firstName" value={formData.firstName} onChange={handleInputChange} />
                         </div>
                         <div className="form-group">
                             <label htmlFor="lastName">Lastname</label>
-                            <input type="text" className="form-control" id="lastName" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+                            <input type="text" className="form-control" id="lastName" name="lastName" value={formData.lastName} onChange={handleInputChange} />
                         </div>
                         <div className="form-group">
                             <label htmlFor="email">Email</label>
-                            <input type="email" className="form-control" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                            <input type="email" className="form-control" id="email" name="email" value={formData.email} onChange={handleInputChange} />
                         </div>
                         <div className="form-group">
                             <label htmlFor="phone">Phone</label>
-                            <input type="text" className="form-control" id="phone" value={phone} onChange={(e) => setPhone(e.target.value)} />
+                            <input type="text" className="form-control" id="phone" name="phone" value={formData.phone} onChange={handleInputChange} />
                         </div>
                         <div className="form-group">
                             <label htmlFor="houseNumber">House number</label>
-                            <input type="text" className="form-control" id="houseNumber" value={houseNumber} onChange={(e) => setHouseNumber(e.target.value)} />
+                            <input type="text" className="form-control" id="houseNumber" name="houseNumber" value={formData.houseNumber} onChange={handleInputChange} />
                         </div>
                         <div className="form-group">
                             <label htmlFor="city">City</label>
-                            <input type="text" className="form-control" id="city" value={city} onChange={(e) => setCity(e.target.value)} />
+                            <input type="text" className="form-control" id="city" name="city" value={formData.city} onChange={handleInputChange} />
                         </div>
                         <div className="form-group">
                             <label htmlFor="street">Street Name</label>
-                            <input type="text" className="form-control" id="street" value={street} onChange={(e) => setStreet(e.target.value)} />
+                            <input type="text" className="form-control" id="street" name="street" value={formData.street} onChange={handleInputChange} />
                         </div>
                         <div className="form-group">
                             <label htmlFor="postalCode">Postal Code</label>
-                            <input type="text" className="form-control" id="postalCode" value={postalCode} onChange={(e) => setPostalCode(e.target.value)} />
+                            <input type="text" className="form-control" id="postalCode" name="postalCode" value={formData.postalCode} onChange={handleInputChange} />
                         </div>
                         <div className="form-group">
                             <label htmlFor="countryCode">Country Code</label>
-                            <input type="text" className="form-control" id="countryCode" value={countryCode} onChange={(e) => setCountryCode(e.target.value)} />
+                            <input type="text" className="form-control" id="countryCode" name="countryCode" value={formData.countryCode} onChange={handleInputChange} />
                         </div>
                         <div className="form-group">
                             <label htmlFor="countryName">Country Name</label>
-                            <input type="text" className="form-control" id="countryName" value={countryName} onChange={(e) => setCountryName(e.target.value)} />
+                            <input type="text" className="form-control" id="countryName" name="countryName" value={formData.countryName} onChange={handleInputChange} />
                         </div>
                         <div className="form-group">
                             <label htmlFor="salutation">Salutation</label>
-                            <select className="form-control" id="salutation" value={salutation} onChange={(e) => setSalutation(e.target.value)}>
+                            <select className="form-control" id="salutation" name="salutation" value={formData.salutation} onChange={handleInputChange}>
                                 <option value="">Choisir...</option>
-                                <option value="Mr">Monsieur</option>
-                                <option value="Ms">Madame</option>
+                                <option value="Mr">Mr</option>
+                                <option value="Mrs">Mrs</option>
                             </select>
                         </div>
                         <div className="form-group">
                             <label htmlFor="pseudo">Pseudo</label>
-                            <input type="text" className="form-control" id="pseudo" value={pseudo} onChange={(e) => setPseudo(e.target.value)} />
+                            <input type="text" className="form-control" id="pseudo" name="pseudo" value={formData.pseudo} onChange={handleInputChange} />
                         </div>
                         <div className="form-group">
                             <label htmlFor="password">Password</label>
-                            <input type="password" className="form-control" id="password"  value={password} onChange={(e) => setPassword(e.target.value)} />
+                            <input type="password" className="form-control" id="password" name="password" value={formData.password} onChange={handleInputChange} />
                         </div>
-                    
                     </div>
                     <div className="modal-footer" style={{ backgroundColor: "#F2F2F2" }}>
-                        
                         <button type="button" className="btn " style={{ backgroundColor: "#4B2E83", color: "#FFF" }} onClick={handleCreateUser}>
                             Create
                         </button>
