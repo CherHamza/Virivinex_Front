78aa02a4d0b0bcbe6cf493b2811d2b765325f535
lana-12 <lana.12@wanadoo.fr>
2024-01-19 16:08:44 +0100

    Modification of the toast message for creation user

--- src/js/components/Individual/IndividualModalCreate.js
+++ src/js/components/Individual/IndividualModalCreate.js
@@ -95,7 +95,7 @@ const IndividualModalCreate = ({ handleShowModal, handleCloseModal, userType }) 
         dataService.createUser(newUser)
             .then(res => {
                 console.log(res);
-                toast.success("User successfully created !");
+                toast.success("Successfully created - Please go to your e-mail to confirm !");
                 setFormData({
                     email: "",
                     pseudo: "",
