00bd3128a768fa18dd5b7bab54fb459b6eec8b6f
CherHamza <hamzacheracher1@hotmail.fr>
2024-01-24 12:05:50 +0100

    display emission on individual page

--- src/js/components/SearchSkus.js
+++ src/js/components/SearchSkus.js
@@ -2,7 +2,7 @@ import React, { useRef, useState, useEffect } from "react";
 import { dataService } from "../services/dataService";
 
 
-const Search = (props) => {
+const SearchSkus = (props) => {
     const [isResultsExists, setIsResultsExists] = useState(false);
     const [results, setResults] = useState([]);
     const searchInput = useRef(null);
@@ -109,5 +109,5 @@ const Search = (props) => {
     );
 };
 
-export default Search;
+export default SearchSkus;
 
