fea0dd4d2dbfc58a9594eb24a4351a49d036ac22
lana-12 <lana.12@wanadoo.fr>
2024-02-29 09:29:58 +0100

    Setup the select to the filter

--- src/js/components/EmissionsAll.js
+++ src/js/components/EmissionsAll.js
@@ -6,6 +6,10 @@ import DefaultImageSrc from '../../../assets/images/bottle1.jpg';
 
 const EmissionsAll = (props) => {
     const [emissionsWithAttributes, setEmissionsWithAttributes] = useState([]);
+    const [selectedOption, setSelectedOption] = useState([]);
+    const [bottleSizes, setBottlesSizes] = useState([]);
+    const [bottleCountry, setBottlesCountry] = useState([]);
+
     const [filterYear, setFilterYear] = useState('');
     const [filterSize, setFilterSize] = useState('');
     const [filterCountry, setFilterCountry] = useState('');
@@ -17,16 +21,40 @@ const EmissionsAll = (props) => {
         const fetchAllEmissions = async () => {
             try {
                 const allEmissions = await emissionService.getAllEmissions();
-
                 const emissionsPromises = allEmissions.map(async (emission) => {
                     const attributes = await dataService.getAttributeValuesFromSellerSKU(emission.id);
                     const selectedOptions = getSelectedOptions(attributes);
-
                     return { emission, attributes, selectedOptions };
                 });
-
+                
                 const emissions = await Promise.all(emissionsPromises);
                 setEmissionsWithAttributes(emissions);
+
+                // Récupérer uniquement le premier tableau d'attributs
+                const firstAttributes = emissions[0].attributes;
+                setSelectedOption(firstAttributes);
+                // console.log('firstAttributes', firstAttributes)
+                
+                //Build array bottleSize
+                const extractUniqueBottleSizesWithId = (attributes) => {
+                    return attributes[17].attribute.options.map(option => ({
+                        id: option.id,
+                        size: option.searchTerms.split(',')[0]
+                    }));
+                };
+                setBottlesSizes(extractUniqueBottleSizesWithId(firstAttributes));
+                
+                //Build array Country
+                const extractUniqueBottleCountry = (attributes) => {
+                    return attributes[19].attribute.options.map(option => ({
+                        id: option.id,
+                        country: option.searchTerms.split(',')[0]
+                    }));
+                };
+                setBottlesCountry(extractUniqueBottleCountry(firstAttributes))
+
+                //Build array Region
+                
             } catch (error) {
                 console.error("Erreur lors de la récupération des émissions :", error);
             }
@@ -51,10 +79,17 @@ const EmissionsAll = (props) => {
 
     const handleSizeChange = (e) => {
         setFilterSize(e.target.value);
+        // console.log('search ', e.target.value)
+        //Recupère id
+        // console.log('search2 ', e.target.selectedOptions[0].id)
     };
 
     const handleCountryChange = (e) => {
         setFilterCountry(e.target.value);
+
+        console.log('search ', e.target.value)
+        //Recupère id
+        console.log('search2 ', e.target.selectedOptions[0].id)
     };
 
     const handleRegionChange = (e) => {
@@ -62,38 +97,57 @@ const EmissionsAll = (props) => {
     };
 
     // Créer un tableau d'années de 1800 à 2024
+    const currentYear = new Date().getFullYear();
+    const startYear = 1800; 
+
     const years = [];
-    for (let year = 1800; year <= 2024; year++) {
+    for (let year = currentYear; year >= startYear; year--) {
         years.push(year);
     }
 
+    // console.log('country ', bottleCountry)
     return (
         <>
-        <div className="filter-options">
+            <div className="filter-options ">
+
             <div className="filter-item">
                 <select value={filterYear} onChange={handleYearChange}>
-                    <option value="">Filter by year</option>
+                        <option value="">Filter by year</option>
                     {years.map((year, index) => (
                         <option key={index} value={year}>{year}</option>
                     ))}
                 </select>
             </div>
-            <div className="filter-item">
-                <input
-                    type="text"
-                    placeholder="Filter by size"
-                    value={filterSize}
-                    onChange={handleSizeChange}
-                />
+          
+            <div className="filter-options">
+                <div className="filter-item">
+                    <label htmlFor="bottleSize">Bottle Size</label>
+                    <select name="bottleSize" className="form-control" defaultValue="" onChange={handleSizeChange}>
+                        <option value="">Please choose a bottle size</option>
+                        {bottleSizes.map((size) => (
+                            <option key={size.id} id={size.id}  value={size.size}>
+                                {size.size || "Please choose a bottle size"}
+                            </option>
+                        ))}
+                    </select>
+                </div>
             </div>
-            <div className="filter-item">
-                <input
-                    type="text"
-                    placeholder="Filter by country"
-                    value={filterCountry}
-                    onChange={handleCountryChange}
-                />
+
+            <div className="filter-options">
+                <div className="filter-item">
+                    <label htmlFor="bottleCountry">Bottle Country</label>
+                        <select name="bottleCountry" className="form-control" defaultValue="" onChange={handleCountryChange}>
+                        <option value="">Please choose a bottle country</option>
+                            {bottleCountry.map((country) => (
+                            <option key={country.id} id={country.id}  value={country.country}>
+                                {country.country || "Please choose a bottle country"}
+                            </option>
+                        ))}
+                    </select>
+                </div>
             </div>
+
+           
             <div className="filter-item">
                 <select value={filterRegion} onChange={handleRegionChange}>
                     <option value="">Select Region</option>
@@ -112,6 +166,7 @@ const EmissionsAll = (props) => {
                     if (filterRegion !== '' && !attributes.some(attr => attr.value === filterRegion)) return false;
                     return true;
                 })
+                   
                 .map(({ emission, attributes, selectedOptions }) => (
                     <div className="card m-4" style={{ width: "18rem" }} key={emission.id}>
                         <img
@@ -142,6 +197,18 @@ const EmissionsAll = (props) => {
                     </div>
                 ))
         }
+            {/* Afficher un message si aucune émission n'est trouvée */}
+            {/* {emissionsWithAttributes
+                .filter(({ emission }) => emission.publishedForSale)
+                .filter(({ attributes }) => {
+                    if (filterYear !== '' && attributes[15].value !== filterYear) return false;
+                    if (filterSize !== '' && !attributes.some(attr => attr.value === filterSize)) return false;
+                    if (filterCountry !== '' && !attributes.some(attr => attr.value === filterCountry)) return false;
+                    if (filterRegion !== '' && !attributes.some(attr => attr.value === filterRegion)) return false;
+                    return true;
+                })
+                .length === 0 && <p>Aucune bouteille trouvée</p>
+            } */}
         </>
     );
 }
